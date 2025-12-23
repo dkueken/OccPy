673fa93e7dac541067d2014cf0a2fd41b871c465
dkueken <daniel.kuekenbrink@gmail.com>
2025-12-23 15:21:28 +0100

    some smaller changes to test scripts, which should actually be moved to sample scripts after merge with master

--- tests/Test_FilterIntersectingPulses.py
+++ tests/Test_FilterIntersectingPulses.py
@@ -1,17 +1,18 @@
-from occpy.OccPy import OccPy
+from occpy.util import filterPointsIntersectingBox, read_trajectory_file
 
 import numpy as np
 
 
-laz_in = r".\data\MLS\OccPy_MLS_test_data.laz"
-traj_in = r".\data\MLS\Rameren_FP05_2022-06-01_07-32-05_results_traj_rot2LV95.txt"
+laz_in = rf".\data\MLS\MLS_TestData_20perc_FP10_2025.laz"
+traj_in = r".\data\MLS\MLS_TestData_traj_FP10_2025.txt"
 
-traj = OccPy.read_trajectory_file(path2traj=traj_in)
+traj = read_trajectory_file(path2traj=traj_in, delimiter=" ", hdr_time="//world_time", hdr_x="x", hdr_y="y", hdr_z="z")
+
 
 minbounds = np.array([1246170, 2676560, 550])
 maxbounds = np.array([1246180, 2676570, 610])
 
 
-pulses_intersecting = OccPy.filterPointsIntersectingBox(laz_in=laz_in, laz_out=f"{laz_in[:-4]}_filteredBox.laz", min_bound=minbounds, max_bound=maxbounds, traj_in=traj, points_per_iter=100000)
+pulses_intersecting = filterPointsIntersectingBox(laz_in=laz_in, laz_out=f"{laz_in[:-4]}_filteredBox.laz", min_bound=minbounds, max_bound=maxbounds, traj_in=traj, points_per_iter=100000)
 
-
+print(pulses_intersecting)
