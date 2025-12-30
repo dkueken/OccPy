37b2b75f2326332cc9d215a2b9b396dc98c2db44
dkueken <daniel.kuekenbrink@gmail.com>
2025-12-30 21:26:49 +0100

    added testing function for TLS use case - ran all scan pos at once, not individual scan pos separately

--- tests/test_occpy.py
+++ tests/test_occpy.py
@@ -54,7 +54,7 @@ def find_project_root(markers=('README.md', '.gitignore', 'environment.yml', 'se
 
     return current  # fallback if nothing found
 
-def test_occpy_run():
+def test_occpy_run_mls():
     from occpy.OccPy import OccPy
 
     # Download test data
@@ -94,7 +94,7 @@ def test_occpy_run():
     # Define sensor position
     test.define_sensor_pos(path2file=settings['ScanPos'],  # Path to the trajectory file
                            is_mobile=True,  # whether acquisition is mobile. Always true for MLS or ULS
-                           single_return=True,  # wheter the data is single or multi return
+                           single_return=True,  # whether the data is single or multi return
                            delimiter=" ",  # delimiter used in the trajectory file
                            hdr_time='//world_time',  # column header for the time information in the trajectory file
                            hdr_x='x',  # column header for the x coordinate in the trajectory file
@@ -133,6 +133,81 @@ def test_occpy_run():
 
     np.testing.assert_array_equal(Classification_norm, Classification_norm_expected)
 
+def test_occpy_run_tls():
+    from occpy.OccPy import OccPy
 
+    # Download test data
+    p = pooch.create(
+        path=pooch.os_cache("occpy_test_data"),
+        base_url="https://zenodo.org/records/18095821/files/",
+        registry={"TLS_demo.zip": "md5:ae9a8de9ff7110595fffe613b2dc83f2"},
+    )
+    p.fetch("TLS_demo.zip", processor=pooch.Unzip(members=["TLS_demo"]), progressbar=True)
+    data_path = os.path.join(p.path, "TLS_demo.zip.unzip", "TLS_demo")
 
+    occpy_root = find_project_root()
+
+    # Load the json settings file
+    with open(os.path.join(occpy_root, 'config', 'settings_TLS_tutorial.JSON'), "r") as file:
+        settings_orig = json.load(file)
+
+    # copy original settings into new variable
+    settings = settings_orig.copy()
+    settings['root_folder'] = os.path.abspath(occpy_root)
+    settings['laz_in'] = os.path.join(data_path, 'LAZ')
+    settings['tif_in']['DTM'] = os.path.join(data_path, 'Grids', 'Ramerenwald_DTM_20250305.tif')
+    settings['tif_in']['DSM'] = os.path.join(data_path, 'Grids', 'Ramerenwald_DSM_20250305.tif')
+    settings['ScanPos'] = os.path.join(data_path, 'ScanPos', 'ScanPositions.txt')
+    settings['out_dir'] = os.path.join(settings['root_folder'], 'output', 'TLS')
+
+    # Initialize OccPy object
+    test = OccPy(laz_in=f"{settings['laz_in']}",
+                 out_dir=settings['out_dir'],
+                 vox_dim=settings['vox_dim'],
+                 lower_threshold=settings['lower_threshold'],
+                 points_per_iter=settings['points_per_iter'],
+                 plot_dim=settings['plot_dim'])
+
+    # Define sensor positions
+    test.define_sensor_pos(path2file=settings['ScanPos'],
+                           is_mobile=False,
+                           single_return=False,
+                           delimiter=",",
+                           hdr_scanpos_id='ID',
+                           hdr_x='X',
+                           hdr_y='Y',
+                           hdr_z='Z',
+                           sens_pos_id_offset=0,
+                           str_idx_ScanPosID=7,
+                           str_end_idx_ScanPosID=10)
+
+    # Run occpy
+    import time
+    tic = time.time()
+    test.do_raytracing()
+    toc = time.time()
+
+    # load Classification npy file
+    Classification = np.load(os.path.join(settings['out_dir'], 'Classification.npy'))
+
+    # Load the expected Classification file from the repository
+    Classification_expected = np.load(os.path.join(data_path, 'Output', 'Classification.npy'))
+
+    np.testing.assert_array_equal(Classification, Classification_expected)
+
+    # Normalize occlusion mapping output
+    from occpy.util import normalize_occlusion_output
+    Nhit_norm, Nmiss_norm, Nocc_norm, Classification_norm, chm = normalize_occlusion_output(
+        input_folder=settings['out_dir'],
+        PlotDim=settings['plot_dim'],
+        vox_dim=settings['vox_dim'],
+        dtm_file=settings['tif_in']['DTM'],
+        dsm_file=settings['tif_in']['DSM'],
+        lower_threshold=settings['lower_threshold'],
+        output_voxels=settings['output_voxels'])
+
+    # Load expected normalized Classification output
+    Classification_norm_expected = np.load(os.path.join(data_path, 'Output', 'Classification_norm.npy'))
+
+    np.testing.assert_array_equal(Classification_norm, Classification_norm_expected)
 
